3ef08a3d609f0c1e8ceb694b429134e71ee6f6f0
WiktorB2004 - Ubuntu <byrka.w@gmail.com>
2023-10-09 01:39:42 +0200

    Implemented file reading, persisting the state of lists, moved funcionalities to dedicated functions

--- main.rs
+++ main.rs
@@ -1,4 +1,8 @@
 use ncurses::*;
+use std::env;
+use std::fs::File;
+use std::io::{BufRead, BufReader, Write};
+use std::process;
 
 const REGULAR_PAIR: i16 = 0;
 const HIGHLIGHT_PAIR: i16 = 1;
@@ -51,7 +55,7 @@ impl UI {
 
     fn end(&mut self) {}
 }
-
+#[derive(Debug)]
 enum Focus {
     Todo,
     Done,
@@ -78,7 +82,71 @@ fn list_down(list: &Vec<String>, list_curr: &mut i32) {
     }
 }
 
+fn list_transfer(list_dest: &mut Vec<String>, list_src: &mut Vec<String>, list_src_curr: &mut i32) {
+    if list_src_curr < &mut (list_src.len() as i32) && *list_src_curr >= 0 {
+        list_dest.push(list_src.remove(*list_src_curr as usize));
+        *list_src_curr -= 1;
+    }
+}
+
+fn parse_item(line: &str) -> Option<(Focus, &str)> {
+    let todo_prefix = "TODO: ";
+    let done_prefix = "DONE: ";
+    if line.starts_with(todo_prefix) {
+        return Some((Focus::Todo, &line[todo_prefix.len()..]));
+    }
+    if line.starts_with(done_prefix) {
+        return Some((Focus::Done, &line[done_prefix.len()..]));
+    }
+    todo!();
+}
+
+fn save_state(todos: &Vec<String>, done: &Vec<String>, file_path: &str) {
+    let mut file = File::create(file_path).unwrap();
+    for todo in todos.iter() {
+        writeln!(file, "TODO: {}", todo).unwrap();
+    }
+    for done in done.iter() {
+        writeln!(file, "DONE: {}", done).unwrap();
+    }
+}
+
+fn load_state(todos: &mut Vec<String>, done: &mut Vec<String>, file_path: &str) {
+    let file = File::open(file_path).unwrap();
+    for (index, line) in BufReader::new(file).lines().enumerate() {
+        match parse_item(&line.unwrap()) {
+            Some((Focus::Todo, title)) => todos.push(title.to_string()),
+            Some((Focus::Done, title)) => done.push(title.to_string()),
+            None => {
+                eprint!("{}:{} ERROR: ill-formed item line", file_path, index + 1);
+                process::exit(1)
+            }
+        }
+    }
+}
+
 fn main() {
+    let mut args = env::args();
+    args.next().unwrap();
+
+    let file_path = {
+        match args.next() {
+            Some(file_path) => file_path,
+            None => {
+                eprintln!("Usage: main.rs <file-path>");
+                eprintln!("ERROR: File path is not provided");
+                process::exit(1);
+            }
+        }
+    };
+
+    let mut todos = Vec::<String>::new();
+    let mut done = Vec::<String>::new();
+    let mut todo_curr: i32 = 0;
+    let mut done_curr: i32 = 0;
+
+    load_state(&mut todos, &mut done, &file_path);
+
     initscr();
     noecho();
     curs_set(CURSOR_VISIBILITY::CURSOR_INVISIBLE);
@@ -89,18 +157,6 @@ fn main() {
 
     refresh();
     let mut quit = false;
-    let mut todos: Vec<String> = vec![
-        "Learn Rust".to_string(),
-        "Learn Bash".to_string(),
-        "Write TODO app".to_string(),
-    ];
-    let mut done: Vec<String> = vec![
-        "Printing list".to_string(),
-        "Have a brekfast".to_string(),
-        "Have lunch".to_string(),
-    ];
-    let mut todo_curr: i32 = 0;
-    let mut done_curr: i32 = -1;
     let mut focus = Focus::Todo;
 
     let mut ui = UI::default();
@@ -110,15 +166,17 @@ fn main() {
         ui.begin(0, 0);
         match focus {
             Focus::Todo => {
-                ui.label("TODO: ", REGULAR_PAIR);
                 ui.begin_list(todo_curr);
+                ui.label("[TODO] DONE", REGULAR_PAIR);
+                ui.label("-------------", REGULAR_PAIR);
                 for (index, todo) in todos.iter().enumerate() {
                     ui.list_element(&format!("- [ ] {}", todo), index as i32);
                 }
                 ui.end_list();
             }
             Focus::Done => {
-                ui.label("DONE: ", REGULAR_PAIR);
+                ui.label("TODO [DONE]", REGULAR_PAIR);
+                ui.label("-------------", REGULAR_PAIR);
                 ui.begin_list(done_curr);
                 for (index, done) in done.iter().enumerate() {
                     ui.list_element(&format!("- [x] {}", done), index as i32)
@@ -142,16 +200,8 @@ fn main() {
                 Focus::Done => list_down(&done, &mut done_curr),
             },
             '\n' => match focus {
-                Focus::Todo => {
-                    if todo_curr < todos.len() as i32 {
-                        done.push(todos.remove(todo_curr as usize));
-                    }
-                }
-                Focus::Done => {
-                    if done_curr < done.len() as i32 {
-                        todos.push(done.remove(done_curr as usize));
-                    }
-                }
+                Focus::Todo => list_transfer(&mut done, &mut todos, &mut todo_curr),
+                Focus::Done => list_transfer(&mut todos, &mut done, &mut done_curr),
             },
             '\t' => {
                 focus = focus.toggle();
@@ -159,5 +209,6 @@ fn main() {
             _ => {}
         }
     }
+    save_state(&todos, &done, &file_path);
     endwin();
 }
